cb4410d86df79607634a5bc6f98301c3971b3de0
uezo <uezo@uezo.net>
2023-02-13 20:45:48 +0900

    v0.2✌️

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpt3-contextual",
-    version=0.1,
+    version=0.2,
     url="https://github.com/uezo/gpt3-contextual",
     author="uezo",
     author_email="uezo@uezo.net",
